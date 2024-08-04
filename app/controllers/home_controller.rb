56beaf46837f6b3ca69c6c0fca14e3c628f5ff5d
Renata Vitoi <renata_band@hotmail.com>
2024-08-04 14:06:20 +0200

    Add home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,6 +25,66 @@ class HomeController < ApplicationController
 
   def impressum
   end
+  
+  def haarschumck
+  end
+
+  def patentspangen-mini-patenspangen
+  end
+
+  def haarspangen
+  end
+
+  def patenspange-aus-kunstharz-textil
+  end
+
+  def haarreifen-haarbander
+  end
+
+  def haarreifen-diamen-metall
+  end
+
+  def haarreifen-aus-kunstoffharz-textil
+  end
+
+  def curlys-haarnadeln
+  end
+
+  def haarschieber-seitenspange
+  end
+
+  def haarkamme-haarklammern
+  end
+
+  def haargreifer-wellenreifen-strahnchenhalter
+  end
+
+  def zopfgummis-zopfruschen
+  end
+
+  def haarchips und magnetchips
+  end
+
+  def accessoires
+  end
+
+  def ringe
+  end
+
+  def ohrclips-ohrhaken-ohrringe-ohrstecker
+  end
+
+  def armbander-armring
+  end
+
+  def broschen
+  end
+
+  def mode-brillen
+  end
+
+  def tasche-clutch
+  end
 end
 
    
